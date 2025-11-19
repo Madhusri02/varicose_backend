0f6a8d614c50f0714706a5fade56a2947fb80951
Madhu_Sri <123156637+Madhusri02@users.noreply.github.com>
2025-11-20 01:11:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,5 +42,8 @@ def predict():
 def home():
     return "Varicose Veins Detection API is running!"
 
-if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False, host='127.0.0.1', port=5000)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
+
